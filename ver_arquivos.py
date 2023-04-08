d09668497ce23e4ee8261e2f6e8bbed69cde83f3
fpdiniz <52112150+fpdiniz@users.noreply.github.com>
2023-04-08 16:04:22 -0300

    Create ver_arquivos.py

---
+++ ver_arquivos.py
@@ -0,0 +1,13 @@
+def new_func():
+    nome = str(input("Enter a name:"))
+    print(f"Olá", nome, "!")
+
+new_func()
+
+import os
+def ver_meu_arquivo(self):
+    abrir = os.open(caminho)
+
+caminho = 'D:\\Custo.txt'
+
+ver_meu_arquivo(caminho)
